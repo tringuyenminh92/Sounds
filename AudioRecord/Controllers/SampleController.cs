e04e93f3655c231d9c5857fde9fd210eef45f529
Tri Nguyen Minh <tringuyenminh92@gmail.com>
2016-09-26 14:22:20 +0700

    Init sounds sample solution

---
+++ AudioRecord/Controllers/SampleController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AudioRecord.Controllers
+{
+    public class SampleController : Controller
+    {
+        //
+        // GET: /Sample/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // ---/RecordRTC/PostRecordedAudioVideo
+        [HttpPost]
+        public ActionResult PostRecordedAudio()
+        {
+            foreach (string upload in Request.Files)
+            {
+                var path = AppDomain.CurrentDomain.BaseDirectory + "/uploads/";
+                var file = Request.Files[upload];
+                if (file == null) continue;
+
+                file.SaveAs(Path.Combine(path, Request.Form[0]));
+            }
+            return Json(Request.Form[0]);
+        }
+
+        // ---/RecordRTC/DeleteFile
+        [HttpPost]
+        public ActionResult DeleteFile()
+        {
+            var fileUrl = AppDomain.CurrentDomain.BaseDirectory + "/uploads/" + Request.Form["delete-file"];
+            new FileInfo(fileUrl + ".wav").Delete();
+            new FileInfo(fileUrl + ".ogg").Delete();
+            new FileInfo(fileUrl + ".webm").Delete();
+            return Json(true);
+        }
+    }
+}
